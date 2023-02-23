b20d07bc88a1cbcca0b56fdd5a01739cd088786a
tuxta <tuxta2@gmail.com>
2023-02-23 20:21:58 +1000

    Removed the evil PyQt5 import

--- Queue.py
+++ Queue.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QSizePolicy
+from PySide6.QtWidgets import QSizePolicy
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl
 from PySide6.QtCore import Qt
@@ -10,6 +10,7 @@ class Queue(QWebEngineView):
         super().__init__(parent)
 
         self.setMinimumSize(1, 1)
+        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
 
         res = requests.get("https://connectkaraoke.com/showlogin.php?ShowDirect=MZSJFQKL")
         key = res.url.split("/")[-2]
@@ -20,6 +21,5 @@ class Queue(QWebEngineView):
         self.refresh_site()
 
     def refresh_site(self):
-        #self.load(QUrl(self.proxy))
-        self.load
+        self.load(QUrl(self.proxy))
         self.show()
