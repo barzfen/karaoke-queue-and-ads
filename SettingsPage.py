6bd64e907e36c1715e790875a9b7a0e46cd4ec18
fenso <fensom80@gmail.com>
2023-04-08 11:01:54 +1000

    removed the restart function

--- SettingsPage.py
+++ SettingsPage.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QWidget
 from PySide6.QtCore import Signal, QSettings
 from Ui_SettingsPage import Ui_SettingsPage
-from utils.RestartApp import restart_app
+
 
 
 class SettingsPage(QWidget):
@@ -34,8 +34,7 @@ class SettingsPage(QWidget):
         self.settings.setValue('Timing/queue', self.ui.queue_time_spinbox.value())
         self.settings.setValue('Timing/promo', self.ui.promo_time_spinbox.value())
         self.done_signal.emit()
-        print("applied changes")
-        restart_app()
+
 
 
     def get_settings(self):
